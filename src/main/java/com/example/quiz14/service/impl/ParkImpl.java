e849cf22426e021f225e83facecb34d032a81c1a
Chang <chang.chen@cloudysys.com>
2025-09-04 13:42:58 +0800

    0904

--- src/main/java/com/example/quiz14/service/impl/ParkImpl.java
+++ src/main/java/com/example/quiz14/service/impl/ParkImpl.java
@@ -1,5 +1,44 @@
 package com.example.quiz14.service.impl;
 
-public class ParkImpl {
+import java.time.LocalDate;
+
+import com.example.quiz14.service.ifs.ParkService;
+import com.example.quiz14.vo.BasicRes;
+import com.example.quiz14.vo.GetAllInfoRes;
+import com.example.quiz14.vo.GetInfoRes;
+import com.example.quiz14.vo.createParkReq;
+import com.example.quiz14.vo.updateParkReq;
+
+public class ParkImpl implements ParkService{
+
+	@Override
+	public BasicRes create(createParkReq req) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public GetInfoRes getInfo(String phone, String carNumber) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public GetAllInfoRes getAllInfo(LocalDate date) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public BasicRes update(updateParkReq req) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public BasicRes delete(String phone) {
+		// TODO Auto-generated method stub
+		return null;
+	}
 
 }
